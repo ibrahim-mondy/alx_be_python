29866f67a9bdb62c9ea01be403338d4f95f03df4
ibrahim-mondy <ebrahimmondy972@gmail.com>
2025-06-08 21:31:18 +0300

    task 2 is done

--- fns_and_dsa/main.py
+++ fns_and_dsa/main.py
@@ -1,4 +1,4 @@
-from arithmetic_operations import  perform_operation
+from arithmetic_operations  import  perform_operation 
 def main():
     print("Arithmetic Operations")
     num1 = (float(input("Enter the first number: ")))
